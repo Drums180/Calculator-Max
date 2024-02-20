fe3c241762bbeed5d50da7c404e85f3581f1e2b3
David Dominguez <118247139+Drums180@users.noreply.github.com>
2024-02-20 17:25:34 -0600

    Update calculator.R

--- calculator.R
+++ calculator.R
@@ -1,18 +1,9 @@
 # Equipo 5 Calculator Max
 # David Dominguez - A01570975
-
 # Eduardo Camacho - A01026437
 
-
-
-
-
-
-
-
-
-#Restar
-restar <- function(a, b) {
+#Restart
+restart <- function(a, b) {
   return(a - b)
 }
 
