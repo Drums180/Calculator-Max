6533c1f74f7167fda9ba5cb09db96d1883cf3029
Drums180 <daviddomor180@hotmail.com>
2024-02-20 17:28:23 -0600

    Menu de Calculadora (Funciones)

--- calculator.R
+++ calculator.R
@@ -1,2 +1,12 @@
 # Equipo 5 Calculator Max
 # David Dominguez - A01570975
+
+# Menú de Calculadora
+menu <- function() {
+  cat("Calculadora en R Studio\n")
+  cat("1. Sumar\n")
+  cat("2. Restar\n")
+  cat("3. Multiplicar\n")
+  cat("4. Dividir\n")
+  cat("5. Salir\n")
+}
