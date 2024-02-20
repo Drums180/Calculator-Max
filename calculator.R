dc92a37464f9547c4bf90a35af27eea9a0f753a9
David Dominguez <118247139+Drums180@users.noreply.github.com>
2024-02-20 17:51:37 -0600

    Update calculator.R

--- calculator.R
+++ calculator.R
@@ -1,27 +1,8 @@
 # Equipo 5 Calculator Max
 # David Dominguez - A01570975
-
-
-
 # Gabriel Medina
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#INTEGRANTE 4
+# Función de División
 dividir <- function(a, b) {
   if (b != 0) {
     return(a / b)
