aeb089596aed3c7b9b2306ebecb94ffe156b8d28
AlejandraS4567 <danielnajera@MacBook-Air-de-Daniel-42.local>
2024-02-20 16:27:54 -0700

    Ale

--- calculator.R
+++ calculator.R
@@ -1,2 +1,22 @@
 # Equipo 5 Calculator Max
 # David Dominguez - A01570975
+
+
+#Alejandra Suarez - A00835247
+
+
+
+
+
+
+
+
+
+
+
+
+
+# INTEGRANTE 3
+multiplicar <- function(a, b) {
+  return(a * b)
+}
