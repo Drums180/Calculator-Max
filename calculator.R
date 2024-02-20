c62dd1fac8c95f3002770bba3d0dee7774bcaeae
Gabriel Medina <gabrielmedina@MacBook-Pro-de-Gabriel-2.local>
2024-02-20 17:31:33 -0600

    Lineas_gabriel

--- calculator.R
+++ calculator.R
@@ -1,2 +1,44 @@
 # Equipo 5 Calculator Max
 # David Dominguez - A01570975
+
+
+
+# Gabriel Medina
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#INTEGRANTE 4
+dividir <- function(a, b) {
+  if (b != 0) {
+    return(a / b)
+  } else {
+    return("No es posible dividir por cero.")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
